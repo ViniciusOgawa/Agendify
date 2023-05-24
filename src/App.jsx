3e00679b19b14a35fed81743658b4a15529036bb
ViniciusOgawa <vini_ogawa@hotmail.com>
2023-05-24 13:34:44 -0300

    initial settings

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { RoutesMain } from "./routes"
+import { GlobalStyle } from "./styles/GlobalStyle"
+
+const App = () => {
+
+  return (
+    <>
+      <GlobalStyle />
+      <RoutesMain />
+    </>
+  )
+}
+
+export { App }
+
+
